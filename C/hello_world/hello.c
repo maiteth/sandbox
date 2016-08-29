df9b77390d81aa1545f7aadcf9009f1657e9f8a1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-08-29 12:10:35 +0200

    Correction faite

--- C/hello_world/hello.c
+++ C/hello_world/hello.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
 
 int main(int argc, char **argv) {
-	for (int i = 0; i < argc; i = i + 2) {
-		printf("arg %d = %s\n", i, argv[i]);
-	}
 	char *name = "Maite";
 	if (argc > 1) {
 		name = argv[1];
 	}
-	
 	printf("Hello World!\n");
 	printf("Salut %s!\n", name);
-	sleep(2);
+
 	return 0;
 }
