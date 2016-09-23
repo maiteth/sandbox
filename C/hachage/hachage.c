22ac3379911a7baf7f8fb00b486c3169a5ac064a
maiteth <maite.thomias@gmail.com>
2016-09-23 15:46:12 +0200

    yuyu

--- C/hachage/hachage.c
+++ C/hachage/hachage.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define HASH_SIZE 500
+#define KEY_SIZE 32
 
 typedef struct _Element Element;
 struct _Element {
-	char key[32];
+	char key[KEY_SIZE];
 	int val;
 	Element *next;
 };
@@ -14,23 +19,83 @@ struct _List {
 
 typedef struct _Hash Hash;
 struct _Hash {
-	List buffer[500];
+	List buffer[HASH_SIZE];
 };
 
+void listAdd(List *l, char *key, int val) {
+	Element *last = (Element *) malloc(sizeof(Element));
+	strncpy(last->key, key, KEY_SIZE);
+	last->val = val;
+	last->next = NULL;
+	if (l->first == NULL) {
+		l->first = last;
+		return;
+	}
+	Element *e = l->first;
+	while (e->next != NULL) {
+		e = e->next;
+	}
+	e->next = last;
+}
+
 void hashInit(Hash *h) {
-	
+	memset(h, 0, sizeof(Hash));
+}
+
+int f(char *key) {
+	int result = 0;
+	for (int i = 0; key[i] == 0; i++) {
+		int c = (int) key[i];
+		result = result + c;
+	}
+	return result;
 }
 
 void hashSet(Hash *h, char *key, int val) {
-	
+	int offset = f(key) % HASH_SIZE;
+	listAdd(&(h->buffer[offset]), key, val);
+}
+
+int listGet(List *l, char *key) {
+	if (l->first == NULL) {
+		//TODO: gerer l'erreur
+		return -1;
+	}
+	Element *e = l->first;
+	while (e) {
+		if (strcmp(key, e->key) == 0) {
+			return e->val;
+		}
+		e = e->next;
+	}
+	//TODO: gerer l'erreur
+	 return -1;
 }
 
 int hashGet(Hash *h, char *key) {
-	return 0;
+	int offset = f(key) % HASH_SIZE;
+	return listGet(&(h->buffer[offset]), key);
+}
+
+void elementPrint(Element *e) {
+	printf("%s -> %d\n", e->key, e->val);
+}
+
+void listPrint(List *l) {
+	if (l->first == NULL) {
+		return;
+	}
+	Element *e = l->first;
+	while (e) {
+		elementPrint(e);
+		e = e->next;
+	}
 }
 
 void hashPrint(Hash *h) {
-	
+	for (int i = 0; i < HASH_SIZE; i++) {
+		listPrint(&(h->buffer[i]));
+	}
 }
 
 int main(int argc, char **argv) {
@@ -40,6 +105,8 @@ int main(int argc, char **argv) {
 	hashSet(&h, "JL", 17);
 	hashPrint(&h);
 	int v = hashGet(&h, "Maite");
+	printf("v = %d\n", v);
+	v = hashGet(&h, "Yannis");
 	printf("v = %d\n", v);
 	return 0;
 }
